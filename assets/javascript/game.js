c702a6976c144ba4eb259d421d39373af1221cb8
elaine <user@host.com>
2018-06-25 22:37:56 -0700

    fixed a click event bug for hero

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,47 +1,43 @@
 $(document).ready(function () {
 
     // declare 4 character vars and their hp
-    var rey = {
-        name: "Rey",
-        hp: 130,
-        damage: 8,
-    };
-    var yoda = {
-        name: "Yoda",
-        hp: 160,
-        damage: 20,
-    };
-    var kyloRen = {
-        name: "Kylo Ren",
-        hp: 110,
-        damage: 5,
-    };
-    var darthVader = {
-        name: "Darth Vader",
-        power: 180,
-        damage: 25,
-    };
+    var characters = {
+        "rey": {
+            name: "Rey",
+            hp: 130,
+            damage: 8,
+        },
+        "yoda": {
+            name: "Yoda",
+            hp: 160,
+            damage: 20,
+        },
+        "kylo": {
+            name: "Kylo Ren",
+            hp: 110,
+            damage: 5,
+        },
+        "vader": {
+            name: "Darth Vader",
+            power: 180,
+            damage: 25,
+        }
+    }
     var characterChosen = false;
     var round = 0;
     var enemyChosen = false;
+    var user = "";
+    var enemy = "";
 
     // functions
 
-    // fight by clicking on attack button
     // hp of enemy goes down, your hp goes down per click
     // "You attacked ENEMY for # damage"
     // "ENEMY attacked you back # damage"
     // as you keep clicking your attack damage increments 
     // when your hp is negative, you lose
     // "You have been defeated. GAME OVER!"
-
     // restart button after you lose
-    function newGame() {
-        characterChosen = false;
-        round = 0;
-
-    }
-
     // when you reduce your defender's hp to negative, "You have defeated ENEMY, you can choose to fight another enemy."
     // when you click on attack after you kill one enemy, "No enemy here."
     // click on remaining enemies, move down to defender div
@@ -53,12 +49,11 @@ $(document).ready(function () {
 
     $(".character").on("click", function () {
 
-        // TO DO: turn this into a function        
+        // TO DO: turn this into a function?      
         // pick 1 character
         if (characterChosen == false && enemyChosen == false && round == 0) {
             $(".choose-character").replaceWith($(this));
             $(this).children().last().css("background-color", "green");
-
 
             // move all divs to the right
             if ($(this).attr("id") == "yoda") {
@@ -74,6 +69,7 @@ $(document).ready(function () {
 
             // div box for "choose your battle" in box-2
             $(".box-2:last").addClass("choose-battle").text("Choose your battle");
+            $(".box-1").css("pointer-events","none");
             characterChosen = true;
             return;
         }
@@ -89,14 +85,33 @@ $(document).ready(function () {
             } else if ($(".box-4").children().length == 0) {
                 $(".box-3").children().appendTo($(".box-4"));
             }
+
+            $(".box-3").append("<img id='attack' src='assets/images/attack.png'>");
         }
 
         enemyChosen = true;
     })
 
+    // fight by clicking on attack button
+    $("#attack").on("click", function () {
+        user = $(".box-1").attr("id");
+        enemy = $(".box-2").attr("id");
 
+        // if id matches, text append var names
+        for (var i = 0; i < characters.length; i++) {
+            if (characters[i] == user) {
 
+            }
+        }
 
+        if (enemy == "rey") {
 
+        } else if (enemy == "yoda") {
 
+        } else if (enemy == "kylo") {
+
+        } else if (enemy == "vader") {
+
+        }
+    })
 })
